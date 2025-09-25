a1c39f356b3a49762aec8e9376f004c8f9800ceb
FP2003 <filippieleckii@gmail.com>
2025-09-25 18:09:21 +0100

    arithmetic logic in go

---
+++ practise_go/arithmetics/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 8
+	b := 4
+
+	fmt.Println("Addition:\t", (a + b))
+	fmt.Println("Subtraction:\t", (a - b))
+	fmt.Println("Multiplication:\t", (a * b))
+	fmt.Println("Division:\t", (a / b))
+	fmt.Println("Remainder:\t", (a % b))
+
+	a++
+	b--
+
+	fmt.Println("Increment:\t", a)
+	fmt.Println("Decrement:\t", b)
+}
