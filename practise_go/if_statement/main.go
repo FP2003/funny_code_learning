96a659ee5f621493f932b928e38750ed0ad612ec
FP2003 <filippieleckii@gmail.com>
2025-10-20 10:48:23 +0100

    if_statement part 1

---
+++ practise_go/if_statement/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	check := 'A'
+
+	if 'A' == check {
+		fmt.Println("\nCharacters Match")
+	} else {
+		fmt.Println("\nCharacters do not match")
+	}
+
+}
