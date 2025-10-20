951d68504498f311fd04287d423e888a2b3b6e95
FP2003 <filippieleckii@gmail.com>
2025-10-20 10:40:29 +0100

    learning precedence

---
+++ practise_go/precedence/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 5 * / % << >> &
+	// 4 + - | ^
+	// 3 == != < <= >=
+	// 2 &&
+	// 1 ||
+
+	sum := 2*3 + 4 - 5
+	fmt.Printf("Default Order: %v \n", sum)
+
+	sum = 2 * ((3 + 4) - 5)
+	fmt.Printf("Forced Order: %v \n\n", sum)
+
+	sum = 7 % 3 * 2
+	fmt.Printf("Default Order: %v \n", sum)
+
+	sum = 7 % (3 * 2)
+	fmt.Printf("Forced Order: %v \n\n", sum)
+
+}
